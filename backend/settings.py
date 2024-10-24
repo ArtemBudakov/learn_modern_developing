f713f9b8820a663484bda43d47c7ca13aa07c2c9
Artem Budakov <a.budakov@napoleonit.com>
2024-10-24 12:48:07 +0300

    ref: changed django project directory

--- backend/settings.py
+++ backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s^i_7cxbmvwa^8)nn36gf)^^_v&&8(j+)bz+zk%zmy_!7f)lay'
+SECRET_KEY = 'django-insecure-0%)3qctk5b@l*v2+eh31%#3c@vz1=cu8(tazz_cv7xo!u@#qq7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
